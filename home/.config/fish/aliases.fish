251517c07ca43bd04e50b5bdfd65ef342e54c944
Feynman Liang <feynman@infinitus.ai>
2019-08-02 12:46:05 -0700

    Update

--- home/.config/fish/aliases.fish
+++ home/.config/fish/aliases.fish
@@ -2,3 +2,5 @@ alias vim "nvim"
 alias ve "vim ~/.config/nvim/init.vim"
 alias te "vim ~/.tmux.conf"
 alias fe "vim ~/.config/fish/config.fish"
+alias ae "vim ~/.config/fish/aliases.fish"
+alias sed gsed
--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,10 +1,15 @@
 fish_vi_key_bindings
 
+# https://github.com/pyenv/pyenv/issues/688
+set -gx GIT_INTERNAL_GETTEXT_TEST_FALLBACKS 1
+
 set -gx PATH ~/bin ~/.homesick/repos/homeshick/bin $PATH
 set -gx GOPATH ~/go
 
 set -gx FZF_DEFAULT_COMMAND  'rg --files --hidden'
 set -gx EDITOR 'nvim'
+
+set -gx CLOUDSDK_PYTHON /usr/local/bin/python2
 
 source ~/.config/fish/aliases.fish
 
