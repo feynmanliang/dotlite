00aceef937bbd94d28d974923dd8755b6d8768da
Feynman Liang <feynman@infinitus.ai>
2019-09-17 19:06:47 -0700

    nvimdiff

--- home/.config/fish/aliases.fish
+++ home/.config/fish/aliases.fish
@@ -4,5 +4,4 @@ alias ve "vim ~/.config/nvim/init.vim"
 alias te "vim ~/.tmux.conf"
 alias fe "vim ~/.config/fish/config.fish"
 alias ae "vim ~/.config/fish/aliases.fish"
-alias sed gsed
 alias vimdiff='nvim -d'
